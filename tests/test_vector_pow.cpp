5c2a849b4e3618e6952049d6dca4693e630192c9
akwhjfdudje <149972547+akwhjfdudje@users.noreply.github.com>
2025-10-19 20:52:40 -0600

    modified test to make more meaningful

--- tests/test_vector_pow.cpp
+++ tests/test_vector_pow.cpp
@@ -26,8 +26,8 @@ TEST(VectorPowKernel, LargeArrays) {
     std::vector<float> A(N), B(N), C(N);
 
     for (int i = 0; i < N; ++i) {
-        A[i] = static_cast<float>(i);
-        B[i] = static_cast<float>(N - i);
+        A[i] = 0.5f + static_cast<float>(i % 100); 
+        B[i] = 0.0f + static_cast<float>(i % 50);   
     }
 
     vectorPow(A.data(), B.data(), C.data(), N);
