88148d55bbc98bc72bdcb4842012c0056417fa5f
akwhjfdudje <149972547+akwhjfdudje@users.noreply.github.com>
2025-12-28 06:50:54 +0300

    added node struct for mcts

--- include/alg/mcts.cuh
+++ include/alg/mcts.cuh
@@ -1,3 +1,8 @@
 #pragma once
 
+struct MCTSNode {
+    float value;        // Accumulated value (reward)
+    int visit_count;    // Number of rollouts (visits)
+};
+
 extern "C" CUDA_KERNELS_API void MCTSRollouts(MCTSNode *nodes, int num_nodes, int rollouts_per_node);
